e9f804278f50120fe5e2d9050acbb844e1d09006
katcozadd <krosecozadd@gmail.com>
2018-03-21 18:07:42 -0600

    Commit 4 - Fizz Buzz answered

--- answers.js
+++ answers.js
@@ -9,7 +9,7 @@
 for ( i = 0; i <= 200; i += 2) {
 	console.log(i);
 }
-*/
+
 
 //Excited Kitten
 // 1. Write code that logs "Love me, pet me! HSSSSSS!" 20 times.
@@ -20,4 +20,34 @@ for ( i = 0; i < 20; i++) {
 // 2. For every even number in your loop, log "...human...why you taking pictures of me?...", "...the catnip made me do it...", or "...why does the red dot always get away..." at random.
 for ( i = 0; i < 20; i += 2) {
 	console.log("...why is life so hard? the human made me get up to eat today!");
+}
+
+//Fizz Buzz
+//1. Write a javascript application that logs all numbers from 1 - 100.
+for ( i = 1; i <= 100; i++) {
+	console.log(i);
+}
+*/
+//2. If a number is divisible by 3 log "Fizz" instead of the number.
+for ( i = 1; i <= 100; i++) {
+	if (i % 3 === 0) {
+		console.log("Fizz");
+	} else {
+
+	}
+}
+
+//3. If a number is divisible by 5 log "Buzz" instead of the number.
+for ( i = 1; i <= 100; i++) {
+	if (i % 3 === 0) {
+		console.log("Fizz");
+	} 
+	if (i % 5 === 0 ) {
+		console.log("Buzz");
+	} 
+	if (i % 3 === 0 && i % 5 === 0) {
+		console.log("FizzBuzz");
+	} else {
+
+	}
 }
