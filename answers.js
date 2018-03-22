867ae6b343f8a08ff8b8c8f59987b8b1ba66c088
katcozadd <krosecozadd@gmail.com>
2018-03-21 18:17:04 -0600

    Commit 5 - Getting to Know You answered

--- answers.js
+++ answers.js
@@ -27,7 +27,7 @@ for ( i = 0; i < 20; i += 2) {
 for ( i = 1; i <= 100; i++) {
 	console.log(i);
 }
-*/
+
 //2. If a number is divisible by 3 log "Fizz" instead of the number.
 for ( i = 1; i <= 100; i++) {
 	if (i % 3 === 0) {
@@ -51,3 +51,33 @@ for ( i = 1; i <= 100; i++) {
 
 	}
 }
+*/
+
+//Getting to Know You
+//1. Matt H. decides that Thom. can't be named "Thom" anymore. Remove "Thom" from the thom array and replace it with "Gameboy".
+const thom = ["Thom", 1000, "Christchurch"]
+const karolin = ["Karolin", 16, "New York"]
+const kristyn = ["Kristyn", 5, "Pittsburgh"]
+const matt = ["Matt H", 186, "Philadelphia"]
+
+thom[0] = "Gameboy";
+console.log(thom);
+
+//2. Karolin just had her birthday; change Karolin's array to reflect her being a year older.
+karolin[1] = 17;
+console.log(karolin);
+
+//3. Change Matt H's hometown from Philadelphia to "Gotham City".
+matt[2] = "Gotham City";
+console.log(matt);
+
+//4. Remove "Pittsburgh" from Kristyn's array and add "Brooklyn".
+kristyn[2] = "Brooklyn";
+console.log(kristyn);
+
+
+
+
+
+
+
